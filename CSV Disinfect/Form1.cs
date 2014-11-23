8ac8d1ca489e63161a864a65e142c67a5de1f421
HalfAdd3r <pconant@gmail.com>
2014-11-23 00:24:07 -0500

    Working File-read

--- CSV Disinfect/Form1.cs
+++ CSV Disinfect/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace CSV_Disinfect
 {
@@ -17,32 +19,71 @@ namespace CSV_Disinfect
             InitializeComponent();
         }
 
+
         private void btnSelect_Click(object sender, EventArgs e)
+        {
+            UseOpenDialog();
+        }
+
+
+        private void UseOpenDialog()
         {
             // Get Users Downloads Path and set as variable
             var envPath = @"%USERPROFILE%\Downloads";
             var filePath = Environment.ExpandEnvironmentVariables(envPath);
-            
+
             // Open File and set global
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
             openFileDialog1.InitialDirectory = filePath;
             openFileDialog1.Filter = "CSV Files (*.csv)|*.csv";
-            
-            openFileDialog1.ShowDialog();
 
-
-            TextReader reader = new StreamReader("triangle.txt");
-            TextWriter writer = new StreamWriter("triangle2.txt");
-            for (; ; )
+            // After click "open" in dialog
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                string s = reader.ReadLine();
-                if (s == null)
-                    break;
-                s = s.Replace(" ", ", ");
-                s = "{" + s + "},";
-                writer.WriteLine(s);
+                try
+                {
+                    readFile(openFileDialog1.OpenFile());
+                } // end Try
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                } // end catch
+            } // Dialog okay
+        } // end UseOpenDialog
+
+
+
+        private void readFile(Stream strmFile)
+        {
+            if (strmFile != null) // not empty
+            {
+                MessageBox.Show("value found");
+                TextReader reader = new StreamReader(strmFile);
+                MessageBox.Show(reader.ReadLine());
+                //for (; ; )
+                //{
+                //    string s = reader.ReadLine();
+                //    if (s == null)
+                //        break;
+                //    s = s.Replace(" ", ", ");
+                //    s = "{" + s + "},";
+                //    writer.WriteLine(s);
+                //}
             }
 
-        }
-    }
+            //TextReader reader = new StreamReader("triangle.txt");
+            //TextWriter writer = new StreamWriter("triangle2.txt");
+            //for (; ; )
+            //{
+            //    string s = reader.ReadLine();
+            //    if (s == null)
+            //        break;
+            //    s = s.Replace(" ", ", ");
+            //    s = "{" + s + "},";
+            //    writer.WriteLine(s);
+            //}
+        } // end readFile
+
+
+    } // End Form1
 }
